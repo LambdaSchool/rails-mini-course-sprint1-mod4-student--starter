684531882000887e2f4a8a134a101646e7e8c781
Mark Oliver <mark.c.oliver@gmail.com>
2019-08-15 17:08:41 -0400

    working

--- archiver/archiver.gemspec
+++ archiver/archiver.gemspec
@@ -38,6 +38,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 2.0"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
-  spec.add_runtime_dependency "activerecord"
+  spec.add_runtime_dependency "activerecord", "~>5.2.3"
   
 end
--- archiver/lib/archiver.rb
+++ archiver/lib/archiver.rb
@@ -1,20 +1,15 @@
 require "archiver/version"
 
 module Archiver
-  class Error < StandardError; end
-  class Archived < ActiveRecord::Base
-    @archived_at = nil
     def archive
-      t = time.new
-      @archived_at = t.inspect
+      update(archived_at: Time.now)
     end
 
     def restore
-      @archived_at = nil
+      update(archived_at: nil)
     end
     
     def archived?
-      @archived_at != nil
+      not archived_at.nil?
     end
-  end
 end
